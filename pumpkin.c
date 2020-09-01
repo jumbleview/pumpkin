e9e8855d100c5848adf066221f451a5132880a1c
jumbleview <jumbleview@gmail.com>
2020-09-01 10:31:01 -0700

    FIle *.atsuo added to .gitignore.  Code refactoring

--- pumpkin.c
+++ pumpkin.c
@@ -1,9 +1,18 @@
 /*
- * pumpkin85.c
+ * pumpkin.c
  *
  * Created: 8/26/2020 6:25:48 PM
  *  Author: Jumbleview
- */ 
+ *
+ *
+ * Program controls two 10mm three-color LED on Attiny85 chip.
+ * LED represents two eyes of Pumpkin Halloween Glitter
+ * Design uses five chip pins: 
+ *    Three attached to LEDs cathodes (the same color cathode of each led to the same pi,n);
+ *    Two pins used to control anodes:  each LED anode attached to the dedicated pin
+ *    Program uses Adam Dunkels Protothreads library
+ *
+ */
 
 #include <avr/io.h>
 //#define 	F_CPU   1000000UL // CKDIV8 set. 1 MHz
@@ -11,106 +20,125 @@
 #include <util/delay.h>
 #include "../pt-1.4/pt.h" // http://dunkels.com/adam/pt/
 
+#define setAllPinOut() DDRB |= (1 << DDB0) | (1 << DDB1) | (1 << DDB2) | (1 << DDB3) | (1 << DDB4)
 
-#define setAllPinOut() \
-	DDRB |= (1 << DDB0) | (1 << DDB1) | (1 << DDB2) | (1 << DDB3) | (1 << DDB4);
+#define pinHigh(bit) PORTB |= (1 << (bit))
 
+#define pinLow(bit) PORTB &= ~(1 << (bit))
 
-#define pinHigh(bit) PORTB |= (1 << (DDB##bit));
+#define	LEFT_ANOD	DDB3
+#define	RIGHT_ANOD	DDB4
 
-#define pinLow(bit) PORTB &= ~(1 << (DDB##bit));
+#define	RED_LED		DDB0
+#define	GREEN_LED	DDB1
+#define	BLUE_LED	DDB2
 
-//  LEFT ANOD bit 3;
-//  RIGHT ANOD bit 4;
-
-//  RED LED bit 0;
-//  GREEN LED bit 1;
-//  BLUE LED bit 2;
+#define PURE_LIMIT 199
+#define MIXED_LIMIT 151
+#define DARK_LIMIT 97
 
 typedef enum {EYE_LEFT, EYE_RIGHT} EYE;
 
 EYE eye = EYE_LEFT;
-void changeEye() {
-	pinHigh(2);
-	pinHigh(1);
-	pinHigh(0);
-	if (eye == EYE_LEFT) {
-		eye = EYE_RIGHT;
-		pinHigh(4);
-		pinLow(3);
-	} else {
-		eye = EYE_LEFT;
-		pinLow(4);
-		pinHigh(3);
-	}
-}
  
 struct pt lpt; // protothread descriptor: left
 struct pt rpt; // protothread descriptor: right
 
+// Pure color procedures
 void greenEye() {
-	pinLow(1);
-	pinHigh(2);
-	pinHigh(0);
+	pinHigh(RED_LED);
+	pinLow(GREEN_LED);
+	pinHigh(BLUE_LED);
 }
 
 void redEye() {
-	pinHigh(2);
-	pinHigh(1);
-	pinLow(0);
+	pinLow(RED_LED);
+	pinHigh(GREEN_LED);
+	pinHigh(BLUE_LED);
 }
 
 void blueEye() {
-	pinLow(2);
-	pinHigh(1);
-	pinHigh(0);
+	pinHigh(RED_LED);
+	pinHigh(GREEN_LED);
+	pinLow(BLUE_LED);
 }
 
-
+// Mixed color procedures
 void whiteEye() {
-	pinLow(2);
-	pinLow(1);
-	pinLow(0);
+	pinLow(RED_LED);
+	pinLow(GREEN_LED);
+	pinLow(BLUE_LED);
 }
 
 void yellowEye() {
-	pinHigh(2);
-	pinLow(1);
-	pinLow(0);
+	pinLow(RED_LED);
+	pinLow(GREEN_LED);
+	pinHigh(BLUE_LED);
 }
 
+void cyanEye() {
+	pinHigh(RED_LED);
+	pinLow(GREEN_LED);
+	pinLow(BLUE_LED);
+}
+
+void magentaEye() {
+	pinLow(RED_LED);
+	pinHigh(GREEN_LED);
+	pinLow(BLUE_LED);
+}
 
 void darkEye() {
-	pinHigh(2);
-	pinHigh(1);
-	pinHigh(0);
+	pinHigh(RED_LED);
+	pinHigh(GREEN_LED);
+	pinHigh(BLUE_LED);
 }
 
-int eyeLeft(struct pt* mlpt)
-{
+// Protothread procedure for left eye
+int eyeLeft(struct pt* mlpt) {
 	static int16_t i=0;
 	PT_BEGIN(mlpt); 	//
-	for (i=0; i < 180; i++) {
-		blueEye();
-		PT_YIELD(mlpt);
-	}
-	for (i=0; i < 180; i++) {
+	for (i=0; i < PURE_LIMIT; i++) {
 		redEye();
 		PT_YIELD(mlpt);
 	}
-	for (i=0; i < 180; i++) {
+	for (i=0; i < PURE_LIMIT; i++) {
 		greenEye();
 		PT_YIELD(mlpt);
 	}
-	for (i=0; i < 180; i++) {
+	for (i=0; i < PURE_LIMIT; i++) {
+		blueEye();
+		PT_YIELD(mlpt);
+	}
+	for (i=0; i < MIXED_LIMIT; i++) {
 		whiteEye();
 		PT_YIELD(mlpt);
 	}
-	for (i=0; i < 180; i++) {
+	for (i=0; i < PURE_LIMIT; i++) {
+		redEye();
+		PT_YIELD(mlpt);
+	}
+	for (i=0; i < MIXED_LIMIT; i++) {
 		yellowEye();
 		PT_YIELD(mlpt);
 	}
-	for (i=0; i < 90; i++) {
+	for (i=0; i < MIXED_LIMIT; i++) {
+		cyanEye();
+		PT_YIELD(mlpt);
+	}
+	for (i=0; i < PURE_LIMIT; i++) {
+		greenEye();
+		PT_YIELD(mlpt);
+	}
+	for (i=0; i < MIXED_LIMIT; i++) {
+		magentaEye();
+		PT_YIELD(mlpt);
+	}
+	for (i=0; i < PURE_LIMIT; i++) {
+		greenEye();
+		PT_YIELD(mlpt);
+	}
+	for (i=0; i < DARK_LIMIT; i++) {
 		darkEye();
 		PT_YIELD(mlpt);
 	}
@@ -118,54 +146,84 @@ int eyeLeft(struct pt* mlpt)
 	PT_END(mlpt);
 }
 
-int eyeRight(struct pt* mlpt)
-{
+// Protothread procedure for right eye
+int eyeRight(struct pt* mlpt) {
 	static int16_t i=0;
 	PT_BEGIN(mlpt); 	
-	for (i=0; i < 200; i++) {
-		redEye();
-		PT_YIELD(mlpt);		
+	for (i=0; i < DARK_LIMIT; i++) {
+		darkEye();
+		PT_YIELD(mlpt);
 	}
-	for (i=0; i < 200; i++) {
+	for (i=0; i < PURE_LIMIT; i++) {
 		greenEye();
 		PT_YIELD(mlpt);
 	}
-	for (i=0; i < 200; i++) {
-		blueEye();
+	for (i=0; i < MIXED_LIMIT; i++) {
+		magentaEye();
 		PT_YIELD(mlpt);
 	}
-	for (i=0; i < 200; i++) {
+	for (i=0; i < PURE_LIMIT; i++) {
+		redEye();
+		PT_YIELD(mlpt);
+	}
+	for (i=0; i < MIXED_LIMIT; i++) {
 		yellowEye();
 		PT_YIELD(mlpt);
 	}
-	for (i=0; i < 200; i++) {
+	for (i=0; i < PURE_LIMIT; i++) {
+		blueEye();
+		PT_YIELD(mlpt);
+	}
+	for (i=0; i < MIXED_LIMIT; i++) {
 		whiteEye();
 		PT_YIELD(mlpt);
 	}
-	for (i=0; i < 100; i++) {
-		darkEye();
+	for (i=0; i < PURE_LIMIT; i++) {
+		greenEye();
+		PT_YIELD(mlpt);
+	}
+	for (i=0; i < PURE_LIMIT; i++) {
+		redEye();
+		PT_YIELD(mlpt);
+	}
+	for (i=0; i < MIXED_LIMIT; i++) {
+		cyanEye();
 		PT_YIELD(mlpt);
 	}
 	PT_RESTART(mlpt);	
 	PT_END(mlpt);
 }
 
-int main(void)
-{
-	// initiate protothread structure...	
+// Function changeEye switches voltage between LEDs and call protothread functions to apply programmed color
+void changeEye() {
+	pinHigh(RED_LED);
+	pinHigh(GREEN_LED);
+	pinHigh(BLUE_LED);
+	if (eye == EYE_LEFT) {
+		eye = EYE_RIGHT;
+		pinHigh(RIGHT_ANOD);
+		pinLow(LEFT_ANOD);
+		// Voltage applied to Right LED 
+		eyeRight(&rpt);
+	} else {
+		eye = EYE_LEFT;
+		pinLow(RIGHT_ANOD);
+		pinHigh(LEFT_ANOD);
+		// Voltage applied to Left LED 		
+		eyeLeft(&lpt);
+	}
+}
+
+int main(void) {
+	// initiate protothread structures
 	PT_INIT(&lpt); 
 	PT_INIT(&rpt); 
-	// program pins for output
+	// Set all pins to output mode
 	setAllPinOut();
 	// main loop
 	while(1) {
 			_delay_ms(2.0);
 			changeEye();
-			if (eye == EYE_LEFT) {
-				eyeLeft(&lpt);
-			} else {
-				eyeRight(&rpt);
-			}
 	}
 }
 
